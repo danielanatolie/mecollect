f026206ea6a8a74b49ac57e158f49a06a0d2a114
Daniel Anatolie <daniel.anatolie@gmail.com>
2019-02-03 12:47:59 -0800

    Changed database from users -> myproperty

--- backend/queries.js
+++ backend/queries.js
@@ -3,7 +3,7 @@ var options = {
   promiseLib: promise
 };
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/users';
+var connectionString = 'postgres://localhost:5432/myproperty';
 var db = pgp(connectionString);
 
 function getAllUsers(req, res, next) {

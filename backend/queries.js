24d9ff55a8b4dd2612396ff3a518c5fea2f232e8
Daniel Anatolie <daniel.anatolie@gmail.com>
2019-02-03 12:34:56 -0800

    Added getAllUsers query

---
+++ backend/queries.js
@@ -0,0 +1,23 @@
+var promise = require('bluebird');
+var options = {
+  promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/users';
+var db = pgp(connectionString);
+
+function getAllUsers(req, res, next) {
+    db.any('select * from user_data')
+          .then(function (data) {
+            res.status(200)
+               .json({
+                  users: data
+              });
+          }).catch(function(err) {
+            return next(err);
+          });
+}
+
+module.exports = {
+    getAllUsers: getAllUsers
+}

d3b919515f193ed432a61e778eeb4ebc4615aa6f
Frances Sin <frances.sin31@gmail.com>
2019-03-07 17:52:18 -0800

    Refactored login stuff into modal.js

--- client/src/App.js
+++ client/src/App.js
@@ -47,12 +47,37 @@ class App extends Component {
     this.setState({ responseToPost: body });
   };
 
-  handleShowModalClick = async () => {this.setState({ showModal: true })};
+  hideLoginModal = async e => {
+    this.setState({
+      showModal: false
+    });
+  }
   
-  handleCloseModal = async () => {this.setState({ showModal: false })};
+  showLoginModal = async e => {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  handleLoginInfo = async (
+    input_signUp,
+    input_email, 
+    input_password, 
+    input_firstname, 
+    input_lastname) => {
+    this.setState({
+      signUp: input_signUp,
+      email: input_email,
+      password: input_password,
+      firstname: input_firstname,
+      lastname: input_lastname
+    }, () => {
+      this.handleLogin();
+    })
+  }
   
-  handleModalSubmit = async e => {
-    e.preventDefault();
+  
+  handleLogin = async () => {
     const response = await fetch("/api/login", {
       method: "POST",
       headers: {
@@ -103,122 +128,17 @@ class App extends Component {
         <p>{this.state.responseToPost}</p>
 
         <div>
-  <ReactBootstrap.Button type="button" onClick={this.handleShowModalClick}>
-    Login
-  </ReactBootstrap.Button>
-  {this.state.showModal ? (
-    <Modal onClose={this.handleCloseModal}>
-      <ReactBootstrap.Modal.Header closeButton onClick={this.handleCloseModal}>
-        <ReactBootstrap.Modal.Title>Sign In</ReactBootstrap.Modal.Title>
-      </ReactBootstrap.Modal.Header>
-      <br />
-      <ReactBootstrap.Form 
-      onSubmit={this.handleModalSubmit}>
-        <ReactBootstrap.Form.Row>
-          <ReactBootstrap.Form.Label>Email</ReactBootstrap.Form.Label>
-          <ReactBootstrap.InputGroup>
-            <ReactBootstrap.Form.Control
-              type="email"
-              placeholder="Email"
-              onChange={e =>
-                this.setState({
-                  email: e.target.value,
-                  signUp: false
-                })
-              }
-            />
-          </ReactBootstrap.InputGroup>
-        </ReactBootstrap.Form.Row>
-        <br />
-        <ReactBootstrap.Form.Row>
-          <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
-          <ReactBootstrap.InputGroup>
-            <ReactBootstrap.Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={e =>
-                this.setState({
-                  password: e.target.value,
-                  signUp: false
-                })
-              }
-            />
-          </ReactBootstrap.InputGroup>
-        </ReactBootstrap.Form.Row>
-        <br />
-        <ReactBootstrap.Button variant="primary" type="submit">
-          Login
+        <ReactBootstrap.Button type="button" onClick={this.showLoginModal}>
+         Login
         </ReactBootstrap.Button>
-        <br/>
-        <ReactBootstrap.Modal.Header>
-        <ReactBootstrap.Modal.Title>Sign Up</ReactBootstrap.Modal.Title>
-      </ReactBootstrap.Modal.Header>
-      <br/>
-      <ReactBootstrap.Form.Row>
-          <ReactBootstrap.Form.Label>Name</ReactBootstrap.Form.Label>
-          <ReactBootstrap.InputGroup>
-            <ReactBootstrap.Form.Control
-              placeholder="First Name"
-              onChange={e =>
-                this.setState({
-                  firstname: e.target.value,
-                  signUp:true
-                })
-              }
-            />
-            <ReactBootstrap.Form.Control
-              placeholder="Last Name"
-              onChange={e =>
-                this.setState({
-                  lastname: e.target.value,
-                  signUp:true
-                })
-              }
-            />
-          </ReactBootstrap.InputGroup>
-        </ReactBootstrap.Form.Row>
-        <br/>
-
-        <ReactBootstrap.Form.Row>
-          <ReactBootstrap.Form.Label>Email</ReactBootstrap.Form.Label>
-          <ReactBootstrap.InputGroup>
-            <ReactBootstrap.Form.Control
-              type="email"
-              placeholder="Email"
-              onChange={e =>
-                this.setState({
-                  email: e.target.value,
-                  signUp:true
-                })
-              }
-            />
-             </ReactBootstrap.InputGroup>
-        </ReactBootstrap.Form.Row>
-        <br/>
-        <ReactBootstrap.Form.Row>
-          <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
-          <ReactBootstrap.InputGroup>
-            <ReactBootstrap.Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={e =>
-                this.setState({
-                  password: e.target.value,
-                  signUp:true
-                })
-              }
-            />
-          </ReactBootstrap.InputGroup>
-        </ReactBootstrap.Form.Row>
-        <br />
-        <ReactBootstrap.Button 
-        variant="primary" 
-        type="submit"
-        >
-        Register</ReactBootstrap.Button>
-      </ReactBootstrap.Form>
-    </Modal>
-  ) : null}
+        { this.state.showModal ? (
+          <Modal 
+            showModal={this.state.showModal} 
+            hideLoginModal={this.hideLoginModal}
+            submitForm={this.handleLoginInfo}
+          />
+        ) : null 
+        } 
   <br />
   <br />
 </div>
--- client/src/modal.js
+++ client/src/modal.js
@@ -1,42 +1,148 @@
+import * as ReactBootstrap from 'react-bootstrap';
+
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+
 import "./modal.css";
 
-class Modal extends Component {
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: false,
+      email: "",
+      password: "",
+      firstname: "",
+      lastname: ""
+    };
+  
+}
 
-
-  componentDidMount() {
-    this.modalTarget = document.createElement("div");
-    this.modalTarget.className = "modal";
-    document.body.appendChild(this.modalTarget);
-    this._render();
-  }
-
-  componentWillUpdate() {
-    this._render();
-  }
-
-  componentWillUnmount() {
-    ReactDOM.unmountComponentAtNode(this.modalTarget);
-    document.body.removeChild(this.modalTarget);
-  }
-
-
-  _render() {
-    ReactDOM.render(
-      <div>
-        <div>
-          {this.props.children}
-          <br/><br/>
-        </div>
-      </div>,
-      this.modalTarget
-    );
+ 
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.props.submitForm(
+      this.state.signUp, 
+      this.state.email, 
+      this.state.password, 
+      this.state.firstname, 
+      this.state.lastname
+      );
   }
 
   render() {
-    return <noscript />;
+    return (
+    <div className="modal">
+     <ReactBootstrap.Modal.Header closeButton onClick={this.props.hideLoginModal}>
+        <ReactBootstrap.Modal.Title>Sign In</ReactBootstrap.Modal.Title>
+      </ReactBootstrap.Modal.Header>
+      <br />
+      <ReactBootstrap.Form 
+      onSubmit={this.handleSubmit}>
+        <ReactBootstrap.Form.Row>
+          <ReactBootstrap.Form.Label>Email</ReactBootstrap.Form.Label>
+          <ReactBootstrap.InputGroup>
+            <ReactBootstrap.Form.Control
+              type="email"
+              placeholder="Email"
+              onChange={e => this.setState({
+                email: e.target.value
+              })}
+            />
+          </ReactBootstrap.InputGroup>
+        </ReactBootstrap.Form.Row>
+        <br />
+        <ReactBootstrap.Form.Row>
+          <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
+          <ReactBootstrap.InputGroup>
+            <ReactBootstrap.Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={e =>
+                this.setState({
+                  password: e.target.value,
+                  signUp: false
+                })
+              }
+            />
+          </ReactBootstrap.InputGroup>
+        </ReactBootstrap.Form.Row>
+        <br />
+        <ReactBootstrap.Button variant="primary" type="submit">
+          Login
+        </ReactBootstrap.Button>
+        <br/>
+        <ReactBootstrap.Modal.Header>
+        <ReactBootstrap.Modal.Title>Sign Up</ReactBootstrap.Modal.Title>
+      </ReactBootstrap.Modal.Header>
+      <br/>
+      <ReactBootstrap.Form.Row>
+          <ReactBootstrap.Form.Label>Name</ReactBootstrap.Form.Label>
+          <ReactBootstrap.InputGroup>
+            <ReactBootstrap.Form.Control
+              placeholder="First Name"
+              onChange={e =>
+                this.setState({
+                  firstname: e.target.value,
+                  signUp:true
+                })
+              }
+            />
+            <ReactBootstrap.Form.Control
+              placeholder="Last Name"
+              onChange={e =>
+                this.setState({
+                  lastname: e.target.value,
+                  signUp:true
+                })
+              }
+            />
+          </ReactBootstrap.InputGroup>
+        </ReactBootstrap.Form.Row>
+        <br/>
+
+        <ReactBootstrap.Form.Row>
+          <ReactBootstrap.Form.Label>Email</ReactBootstrap.Form.Label>
+          <ReactBootstrap.InputGroup>
+            <ReactBootstrap.Form.Control
+              type="email"
+              placeholder="Email"
+              onChange={e =>
+                this.setState({
+                  email: e.target.value,
+                  signUp:true
+                })
+              }
+            />
+             </ReactBootstrap.InputGroup>
+        </ReactBootstrap.Form.Row>
+        <br/>
+        <ReactBootstrap.Form.Row>
+          <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
+          <ReactBootstrap.InputGroup>
+            <ReactBootstrap.Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={e =>
+                this.setState({
+                  password: e.target.value,
+                  signUp:true
+                })
+              }
+            />
+          </ReactBootstrap.InputGroup>
+        </ReactBootstrap.Form.Row>
+        <br />
+        <ReactBootstrap.Button 
+        variant="primary" 
+        type="submit"
+        >
+        Register</ReactBootstrap.Button>
+      </ReactBootstrap.Form>
+    </div>
+    
+    )
   }
+  
 }
 
-export default Modal;
+
